4a95881c216e9178a4a134401f4f962347f0b971
MAYUR RAJPUT <mayurrajput8486@gmail.com>
2023-12-29 15:34:08 +0530

    first commit in main file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
         <h1>We are learning Fetch Method in ReactJS</h1>
+        <h1>We are learning Promise in ReactJS</h1>
         <h1>I am Fetching Data from API</h1>
         {/* <Fetchapi/> */}
         {/* <Todoapi/> */}
